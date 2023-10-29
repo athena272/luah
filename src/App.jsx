afabbfd568f14a79118281e6972ae5f95b602333
athena272 <guilhermera272@gmail.com>
2023-10-29 07:54:49 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,8 +38,8 @@ const products = [
     size: ["M", "GG"],
     categories: ["tShirt", "novidades"],
     image: [
-      "media/roupas/cpAzul.jpeg",
-      "media/roupas/cpAzul2.jpeg",
+      "media/roupas/Flores.jpeg",
+      "media/roupas/Flores2.jpeg",
     ],
     description: "🧣 T-Shirt Flores.",
     descSize: "📐 Tamanhos M e GG disponíveis",
